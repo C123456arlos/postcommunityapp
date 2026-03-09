a75073cb9b5ea682a5dd7b4d5d5b5417b95bdd36
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-03-09 09:02:32 -0400

    first commit

--- src/components/Navbar/Directory/MenuListItem.tsx
+++ src/components/Navbar/Directory/MenuListItem.tsx
@@ -1,7 +1,7 @@
 import useDirectory from '@/src/hooks/useDirectory'
 import { Flex, Icon, Image, MenuItem } from '@chakra-ui/react'
 import React from 'react'
-import { IconType } from 'react-icons'
+// import { IconType } from 'react-icons'
 import { ElementType } from 'react'
 
 type Props = {
@@ -12,7 +12,7 @@ type Props = {
     imageURL?: string
 }
 
-const MenuListItem = ({ displayText, link, icon, iconColor, imageURL }: Props) => {
+const MenuListItem = ({ displayText, link, icon: Icon, iconColor, imageURL }: Props) => {
     const { onSelectMenuItem } = useDirectory()
     return (
         <MenuItem width={'100%'} fontSize={'10pt'} _hover={{ bg: 'gray.100' }} onClick={() => onSelectMenuItem({ displayText, link, icon, iconColor, imageURL })}>
