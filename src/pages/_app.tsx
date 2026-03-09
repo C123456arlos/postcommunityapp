dbd75f35ff7080f09f295d177b0a83e1d8cb5ec2
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-03-09 16:20:21 -0400

    first commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,13 @@ import { theme } from '../chakra/theme'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout/Layout'
 import { RecoilRoot } from 'recoil'
+import { Icon } from '@chakra-ui/icons'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
       <ChakraProvider theme={theme} >
-        <Layout>
+        <Layout icon={Icon}>
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
